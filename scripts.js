ae0204e16f219d743b46e30e5625fc62e79e11d8
Jonathan97-web <zakrisson97@gmail.com>
2023-02-16 21:28:42 +0000

    Add countdown timer JS and fixed media queries

--- scripts.js
+++ scripts.js
@@ -72,3 +72,29 @@ function flipCard() {
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 /* Memory board End */
+
+/* Timer for memory game */
+
+const timeH = document.querySelector('.timer-memory')
+let timeSecond = 90;
+
+displayTime(timeSecond)
+
+const countDown = setInterval (()=>{
+  timeSecond--;
+  displayTime(timeSecond);
+  if(timeSecond <= 0  ||  timeSecond < 1){
+    endTime();
+    clearInterval(countDown);
+  }
+},1000)
+
+function displayTime(second){
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `${min<10 ? '0': ''}${min}:${sec<10 ? '0':''}${sec}`
+}
+
+function endTime() {
+  timeH.innerHTML = 'GAME OVER'
+}
