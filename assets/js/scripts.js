583227b1fa56ae0a6c0443afdca3dd19234f9011
Jonathan97-web <zakrisson97@gmail.com>
2023-02-21 13:20:48 +0000

    Fix format of code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,15 +7,15 @@ let modal = document.getElementById("modal-memory");
 let btn = document.getElementById("modal-button");
 let span = document.getElementsByClassName("close-modal")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
